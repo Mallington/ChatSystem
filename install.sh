63285b511be10820f68acc0ca11d93dcf90f53a5
Mathew Allington <funkymallington@gmail.com>
2019-04-30 19:59:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-read -p "Would you like to install Chat System?"
+read -p "Would you like to install Chat System? (Y/N) "
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
+   echo "Exiting..."
     exit 1
 else
    echo "Installing the latest version of Mallington/ChatSystem from github."
