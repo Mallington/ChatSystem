3e8722a889130bc03f0cd2455cd988615cb038be
Mathew Allington <funkymallington@gmail.com>
2019-04-30 19:30:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "Installing chat system"
-
-echo "This feature is not yet complete."
+read -p "Are you sure you want to install? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+else
+   echo "Installing"
+fi
